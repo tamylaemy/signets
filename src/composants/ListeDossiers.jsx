a2df43648a504803ea4ba07844d50d7cafbf7370
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-03-30 21:01:36 -0400

    Firebase installé

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,6 +1,12 @@
 import './ListeDossiers.scss';
 import dossTab from '../data/liste-dossiers.json';
 import Dossier from './Dossier';
+import firebase from 'firebase/app';
+import firestore from 'firebase/firestore';
+
+const firebaseConfig = {
+
+};
 
 export default function ListeDossiers() {
   return (
