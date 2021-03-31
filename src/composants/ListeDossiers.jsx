7007a1dd00e4cb3ed5c8ba0f3fd2f759fad80893
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-03-30 21:06:51 -0400

    Ajout firebaseConfig

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -5,7 +5,12 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAZK-S_nidq0I8AdTnT2_duj7GXE0txxCo",
+  authDomain: "iwra-react-f1315.firebaseapp.com",
+  projectId: "iwra-react-f1315",
+  storageBucket: "iwra-react-f1315.appspot.com",
+  messagingSenderId: "93876236827",
+  appId: "1:93876236827:web:71f57fb14579e0b4f0bebe"
 };
 
 export default function ListeDossiers() {
