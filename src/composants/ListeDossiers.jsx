b6d45593b84c437f815e23211820777d6e1d7e5b
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-03-30 22:30:14 -0400

    Première intégration Firestore complétée

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,18 +1,31 @@
 import './ListeDossiers.scss';
-import dossTab from '../data/liste-dossiers.json';
+// import dossTab from '../data/liste-dossiers.json';
 import Dossier from './Dossier';
-import firebase from 'firebase/app';
-import firestore from 'firebase/firestore';
-
-const firebaseConfig = {
-
-};
+import { useEffect, useState } from 'react';
+import dbFirestore from '../data/firebase';
 
 export default function ListeDossiers() {
+  // État des dossiers de signets
+  const [dossiers, setDossiers] = useState([]);
+
+  // Étape C : Exécuter une requête sur la collection 'dossiers-temp' pour lire l'info des dossiers disponibles
+  // Comme le code suivant fait appel à une ressource externe, il est ASYNCHRONE
+  // donc, il n'y a pas de garantie que le tableau dossiers soit remplit avant l'affichage du composant
+  useEffect(
+    () => dbFirestore.collection('dossiers-temp').get().then(
+            reponse => {
+              let dossiersTemp = [];
+              reponse.forEach(doc => dossiersTemp.push({id: doc.id, ...doc.data()}));
+              setDossiers(dossiersTemp);
+            }
+          )
+    , []
+  );
+
   return (
     <ul className="ListeDossiers">
       {
-        dossTab.map( 
+        dossiers.map( 
           dossier =>  <li key={dossier.id}><Dossier {...dossier} /></li>
         )
       }
---
+++ src/data/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import firestore from 'firebase/firestore';
+
+// objet de configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAZK-S_nidq0I8AdTnT2_duj7GXE0txxCo",
+    authDomain: "iwra-react-f1315.firebaseapp.com",
+    projectId: "iwra-react-f1315",
+    storageBucket: "iwra-react-f1315.appspot.com",
+    messagingSenderId: "93876236827",
+    appId: "1:93876236827:web:71f57fb14579e0b4f0bebe"
+  };
+
+ // Étape A : Initialiser Firebase
+if(firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+  // Étape B : Initialiser Firestore
+  const dbFirestore = firebase.firestore();
+  
+  // Exporter par défaut la référence à Firestore
+  export default dbFirestore;
