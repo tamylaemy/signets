e4979933fbb4aadb4a4a83faf50cb52f17217795
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-05-10 17:53:29 -0400

    Firebase Hosting - not working

--- src/composants/Accueil.jsx
+++ src/composants/Accueil.jsx
@@ -8,7 +8,7 @@ export default function Accueil() {
 
   return (
     <div className="Accueil">
-      <h3 className="logo">Signets <span>(beta)</span></h3>
+      <h3 className="logo">Signets <span>()</span></h3>
       <h2 className="amorce">Organisez vos signets Web, <br/>Un jeu d'enfant</h2>
       <div id="fbui"></div>
     </div>
